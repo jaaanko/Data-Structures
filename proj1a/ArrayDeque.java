44a9fde4861dd22d6f0a8f0f48bde6500d445962
jaaanko <janko2454@gmail.com>
2018-12-01 17:24:50 +0800

    Implement resize function

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -12,12 +12,18 @@ public class ArrayDeque <T>{
     }
 
     public void addFirst(T item){
+        if(size == items.length){
+            resize(size * 2);
+        }
         items[nextFirst] = item;
         size++;
         nextFirst = minusOne(nextFirst);
     }
 
     public void addLast(T item){
+        if(size == items.length){
+            resize(size * 2);
+        }
         items[nextLast] = item;
         size++;
         nextLast = plusOne(nextLast);
@@ -32,6 +38,9 @@ public class ArrayDeque <T>{
     }
 
     public T removeLast(){
+        if(items.length >= 16 && (size * 1.0)/items.length < 0.25){
+            resize(items.length / 2);
+        }
         T oldLast = items[minusOne(nextLast)];
         items[minusOne(nextLast)] = null;
         size--;
@@ -40,6 +49,9 @@ public class ArrayDeque <T>{
     }
 
     public T removeFirst(){
+        if(items.length >= 16 && (size * 1.0)/items.length < 0.25){
+            resize(items.length / 2);
+        }
         T oldFirst = items[plusOne(nextFirst)];
         items[plusOne(nextFirst)] = null;
         size--;
@@ -85,6 +97,18 @@ public class ArrayDeque <T>{
             x = x - items.length;
         }
         return x;
+    }
+
+    private void resize(int capacity){
+        T a[] = (T[]) new Object[capacity];
+        int start = plusOne(nextFirst);
+        System.arraycopy(items,start,a,0,size - start);
+        if(start != 0){
+            System.arraycopy(items,0,a,size-start,start);
+        }
+        nextLast = size;
+        items = a;
+        nextFirst = items.length - 1;
     }
 
 }
